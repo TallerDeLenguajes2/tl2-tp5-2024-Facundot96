4fa7c40c4f212efe31c806d57be0178679c4cfd4
Facundot96 <54074856+Facundot96@users.noreply.github.com>
2024-11-01 06:40:59 -0300

    se agregan entidades

---
+++ TP5/Models/Budget.cs
@@ -0,0 +1,39 @@
+﻿namespace TP5.Models;
+
+public class Budget
+{
+    private int idBudget;
+    private string clientName;
+    private List<BudgetProductDetail> details;
+
+    public Budget(int idBudget, string clientName, List<BudgetProductDetail> details)
+    {
+        this.idBudget = idBudget;
+        this.clientName = clientName;
+        this.details = details;
+    }
+
+    public Budget()
+    {
+    }
+
+    public int IdBudget
+    {
+        get => idBudget;
+        set => idBudget = value;
+    }
+
+    public string ClientName
+    {
+        get => clientName;
+        set => clientName = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public List<BudgetProductDetail> Details
+    {
+        get => details;
+        set => details = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    
+    
+}
--- TP5/obj/Debug/net8.0/TP5.AssemblyInfo.cs
+++ TP5/obj/Debug/net8.0/TP5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2a485f566a542bd41c504de527c3dd19dd1f33f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52a65089300abd9fe449205423f70ce4c154b373")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
